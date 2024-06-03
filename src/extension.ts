8370af16c3529073041cd25fc91c886b66d763d8
Melkz Siqueira <melkz.siqueira@gmail.com>
2024-06-02 21:20:03 -0300

    feature: implements the extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,135 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as https from 'https';
+import { promisify } from 'util';
+import { pipeline, Readable } from 'stream';
+import { getUri } from './utils/utils';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const streamPipeline = promisify(pipeline);
+
 export function activate(context: vscode.ExtensionContext) {
+  const appServerViewProvider = new AppServerViewProvider(context);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "artefatvs" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('artefatvs.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from artefatvs!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider(
+      AppServerViewProvider.viewType,
+      appServerViewProvider
+    )
+  );
 }
 
-// This method is called when your extension is deactivated
+class AppServerViewProvider implements vscode.WebviewViewProvider {
+  public static readonly viewType = 'appserverSidebar';
+  private readonly _extensionUri: vscode.Uri;
+  private readonly _extensionTestMode: boolean;
+
+  constructor(private readonly _context: vscode.ExtensionContext) {
+    this._extensionUri = this._context.extensionUri;
+    this._extensionTestMode =
+      this._context.extensionMode === vscode.ExtensionMode.Test;
+  }
+
+  public resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    context: vscode.WebviewViewResolveContext,
+    _token: vscode.CancellationToken
+  ) {
+    webviewView.webview.options = {
+      enableScripts: true,
+      localResourceRoots: [this._extensionUri],
+    };
+
+    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+
+    webviewView.webview.onDidReceiveMessage(async (message) => {
+      switch (message.command) {
+        case 'requestDownloadButton':
+          const folderUri = await vscode.window.showOpenDialog({
+            canSelectFolders: true,
+            canSelectFiles: false,
+            canSelectMany: false,
+          });
+
+          if (folderUri && folderUri.length > 0) {
+            this.downloadFile(folderUri[0].fsPath);
+          }
+          break;
+      }
+    });
+  }
+
+  private _getHtmlForWebview(webview: vscode.Webview): string {
+    const toolkitUri = getUri(webview, this._extensionUri, [
+      'node_modules',
+      '@vscode',
+      'webview-ui-toolkit',
+      'dist',
+      !this._extensionTestMode ? 'toolkit.min.js' : 'toolkit.js',
+    ]);
+    const scriptArtifacts = getUri(webview, this._extensionUri, [
+      'media',
+      'scriptArtifacts.js',
+    ]);
+    const styleUri = getUri(webview, this._extensionUri, [
+      'media',
+      'styleArtifacts.css',
+    ]);
+    const nonce = getNonce();
+
+    return `<!DOCTYPE html>
+            <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <script type="module" src="${toolkitUri}"></script>
+                    <script type="module" src="${scriptArtifacts}"></script>
+                    <link rel="stylesheet" href="${styleUri}">
+                    <title>Artefatos</title>
+                </head>
+                <body id="webview-body">
+                    <section class="component">
+                        <p>AppServer</p>
+                        <vscode-divider role="separator"></vscode-divider>
+                    </section>
+
+                    <section class="component-row">
+                        <section class="component">
+                            <vscode-dropdown id="select-version-option">
+                                <vscode-option>latest</vscode-option>
+                            </vscode-dropdown>
+                        </section>
+
+                        <section class="component">
+                            <vscode-button id="download-button">Download</vscode-button>
+                        </section>
+                    </section>
+                </body>
+            </html>`;
+  }
+
+  private async downloadFile(folderPath: string) {
+    const url = process.env.APPSERVER_URL ?? '';
+    const filePath = path.join(folderPath, 'appserver.zip');
+
+    const file = fs.createWriteStream(filePath);
+    const request = https.get(url, (response) => {
+      response.pipe(file);
+    });
+
+    await streamPipeline(request as unknown as Readable, file);
+    vscode.window.showInformationMessage('Download complete!');
+  }
+}
+
+function getNonce() {
+  let text = '';
+  const possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 32; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
 export function deactivate() {}

fed752bea51467e992a6ddb723f196d8072478bb
Melkz Siqueira <melkz.siqueira@gmail.com>
2024-06-08 19:30:56 -0300

    feat: add artifact download with progress bar and open file location option after complete

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,7 @@ import * as vscode from "vscode";
 import * as fs from "fs";
 import * as path from "path";
 import * as https from "https";
-import { promisify } from "util";
-import { pipeline, Readable } from "stream";
 import { getUri } from "./utils/utils";
-
-const streamPipeline = promisify(pipeline);
 
 export function activate(context: vscode.ExtensionContext) {
   const appServerViewProvider = new AppServerViewProvider(context);
@@ -21,8 +17,13 @@ export function activate(context: vscode.ExtensionContext) {
 
 class AppServerViewProvider implements vscode.WebviewViewProvider {
   public static readonly viewType = "appserver";
+
   private readonly _extensionUri: vscode.Uri;
   private readonly _extensionTestMode: boolean;
+  private _binary: string = "";
+  private _os: string = "";
+  private _architecture: string = "";
+  private _version: string = "";
 
   constructor(private readonly _context: vscode.ExtensionContext) {
     this._extensionUri = this._context.extensionUri;
@@ -52,8 +53,28 @@ class AppServerViewProvider implements vscode.WebviewViewProvider {
           });
 
           if (folderUri && folderUri.length > 0) {
-            this.downloadFile(folderUri[0].fsPath);
+            this.download(folderUri[0].fsPath);
           }
+
+          break;
+
+        case "requestBinaryOption":
+          this._binary = message.option;
+
+          break;
+
+        case "requestOSOption":
+          this._os = message.option;
+
+          break;
+
+        case "requestArchitectureOption":
+          this._architecture = message.option;
+
+          break;
+
+        case "requestVersionOption":
+          this._version = message.option;
 
           break;
       }
@@ -92,29 +113,29 @@ class AppServerViewProvider implements vscode.WebviewViewProvider {
                   <section class="component">
                     <label for="my-dropdown">Binário:</label>
                     <vscode-dropdown id="select-binary-option">
-                      <vscode-option>panthera_onca</vscode-option>
                       <vscode-option>harpia</vscode-option>
+                      <vscode-option selected>panthera_onca</vscode-option>
                     </vscode-dropdown>
                   </section>
             
                   <section class="component">
                     <label for="my-dropdown">Sistema operacional:</label>
                     <vscode-dropdown id="select-os-option">
-                      <vscode-option>windows</vscode-option>
+                      <vscode-option selected>windows</vscode-option>
                     </vscode-dropdown>
                   </section>
             
                   <section class="component">
                     <label for="my-dropdown">Arquitetura:</label>
                     <vscode-dropdown id="select-architecture-option">
-                      <vscode-option>64</vscode-option>
+                      <vscode-option selected>64</vscode-option>
                     </vscode-dropdown>
                   </section>
             
                   <section class="component">
                     <label for="my-dropdown">Versão:</label>
                     <vscode-dropdown id="select-version-option">
-                      <vscode-option>latest</vscode-option>
+                      <vscode-option selected>latest</vscode-option>
                     </vscode-dropdown>
                   </section>
             
@@ -126,42 +147,81 @@ class AppServerViewProvider implements vscode.WebviewViewProvider {
             </html>`;
   }
 
-  private async downloadFile(folderPath: string) {
+  private async download(folderPath: string) {
+    const fileName = "appserver.zip";
+
     try {
-      const url = `${process.env.BASE_URL}/tec/appserver/harpia/windows/64/latest/appserver.zip`;
+      const url = `${process.env.BASE_URL}/tec/appserver/${this._binary}/${this._os}/${this._architecture}/${this._version}/${fileName}`;
       const options = {
         headers: {
           Authorization: process.env.AUTH_TOKEN,
         },
       };
-      const filePath = path.join(folderPath, "appserver.zip");
+      const filePath = path.join(folderPath, fileName);
       const file = fs.createWriteStream(filePath);
-      const request = https.get(url, options, (response) => {
-        if (response.statusCode !== 200) {
-          vscode.window.showErrorMessage(
-            `Failed to download file: ${response.statusCode}`
-          );
-          return;
+
+      await vscode.window.withProgress(
+        {
+          location: vscode.ProgressLocation.Notification,
+          title: `${fileName} downloading...`,
+          cancellable: true,
+        },
+        async (progress, token) => {
+          token.onCancellationRequested(() => {
+            vscode.window.showInformationMessage(
+              `User canceled the long running ${fileName} download operation`
+            );
+          });
+
+          return new Promise<void>((resolve, reject) => {
+            const request = https.get(url, options, (response) => {
+              const contentLength = response.headers["content-length"] ?? 0;
+              const totalLength =
+                typeof contentLength === "number"
+                  ? contentLength
+                  : parseInt(contentLength, 10);
+
+              if (response.statusCode !== 200) {
+                vscode.window.showErrorMessage(
+                  `Failed to download ${fileName}: ${response.statusCode}`
+                );
+                return reject();
+              }
+
+              response.on("data", (chunk) => {
+                progress.report({
+                  increment: (chunk.length / totalLength) * 100,
+                });
+              });
+              response.pipe(file);
+            });
+
+            request.on("error", (err) => {
+              vscode.window.showErrorMessage(`${fileName} download request error: ${err.message}`);
+              reject(err);
+            });
+
+            file.on("finish", async () => {
+              const openButton = 'Open file location';
+              const selectedButton = await vscode.window.showInformationMessage(
+                `${fileName} download complete!`,
+                openButton
+              );
+
+              if (selectedButton === openButton) {
+                vscode.commands.executeCommand('revealFileInOS', vscode.Uri.file(filePath));
+              }
+
+              resolve();
+            });
+
+            file.on("close", resolve);
+            file.on("error", reject);
+          });
         }
-        response.pipe(file);
-      });
-
-      request.on("error", (err) => {
-        vscode.window.showErrorMessage(`Request error: ${err.message}`);
-      });
-
-      file.on("finish", () => {
-        vscode.window.showInformationMessage("Download complete!");
-      });
-
-      await new Promise((resolve, reject) => {
-        file.on("close", resolve);
-        file.on("error", reject);
-      });
+      );
     } catch (error) {
-      vscode.window.showErrorMessage(`Download failed: ${error}`);
+      vscode.window.showErrorMessage(`${fileName} download failed: ${error}`);
     }
   }
 }
-
-export function deactivate() {}
